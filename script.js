4383eccaf41d53d53901468b7794a84e799f412f
Patriciamwaura <patriciamwaura01@gmail.com>
2026-04-13 19:55:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,21 +12,46 @@ if (saved){
     editor.value=saved;
     output.innerHTML=sanitized(saved);
 };
-editor.addEventListener("input",() => {
-    const content=editor.value;
-    output.innerHTML=sanitized (content);
-    localStorage.setItem("draft", content);
-});
 window.preview =function(){
     const content= editor.value;
-    output.innerHTML=sanitized(content);
+    output.innerHTML= sanitized(content);
 localStorage.setItem("draft", content);
 };
-window.clearDraft=function(){
-    const editor=document.getElementById("editor");
-    const output=document.getElementById("output");
-    editor.value="";
-    output.innerHTML="";
-    localStorage.removeItem("draft");
-};
-});
+    window.confirmClear=function(){
+        const confirmBox=document.getElementById("confirmClear");
+        confirmBox.innerHTML=`
+        <p>Are you sure you want to clear your draft?</p>
+        <button onclick="handleClear(true)">Yes</button>
+        <button onclick="handleClear(false)">No</button>
+       `
+    };
+editor.addEventListener("input",()=>{
+        const content=editor.value;
+        output.innerHTML=sanitized(content);
+        localStorage.setItem("draft",content);
+        toggleClearButton();
+    });
+    function toggleClearButton() {
+    const clearBtn = document.getElementById("clearBtn");
+    const saved = localStorage.getItem("draft");
+
+    if (saved && saved.trim() !== "") {
+        clearBtn.disabled = false;
+    } else {
+        clearBtn.disabled = true;
+    }
+}
+    window.handleClear=function(choice){
+        const editor=document.getElementById("editor");
+        const output=document.getElementById("output");
+        const confirmBox=document.getElementById("confirmClear");
+        if(choice){
+            editor.value="";
+            output.innerHTML="";
+            localStorage.removeItem("draft");
+            toggleClearButton();
+        };
+confirmBox.innerHTML=""
+    }
+    });
+
